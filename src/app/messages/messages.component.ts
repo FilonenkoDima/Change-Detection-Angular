150f359b599f6d2dba996bbf6fa1301e7a813774
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-01 12:44:28 +0200

    added new message component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NewMessageComponent } from './new-message/new-message.component';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [NewMessageComponent],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
 export class MessagesComponent {
+  messages = signal<string[]>([]);
+
   get debugOutput() {
     console.log('[Messages] "debugOutput" binding re-evaluated.');
     return 'Messages Component Debug Output';
+  }
+
+  onAddMessage(message: string) {
+    this.messages.update((oldMessages) => [...oldMessages, message]);
   }
 }
