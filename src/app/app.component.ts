8f4d43f7e2ca509cb2cbe88037d372595170b5f3
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-01 12:22:55 +0200

    added messages component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent],
+  imports: [CounterComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
