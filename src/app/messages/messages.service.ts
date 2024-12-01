65af266e74943074ff0b9a4ce0e0192cf803828d
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-01 17:44:43 +0200

    change detection mannually with RxJs

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,13 +1,23 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  private messages = signal<string[]>([]);
-  allMessages = this.messages.asReadonly();
+  messages$ = new BehaviorSubject<string[]>([]);
+  // private messages = signal<string[]>([]);
+  private messages: string[] = [];
+  get allMessages() {
+    return [...this.messages];
+  }
+  // allMessages = this.messages.asReadonly();
 
+  // addMessage(message: string) {
+  //   this.messages.update((prevMessages) => [...prevMessages, message]);
+  // }
   addMessage(message: string) {
-    this.messages.update((prewMessages) => [...prewMessages, message]);
+    this.messages = [...this.messages, message];
+    this.messages$.next([...this.messages]);
   }
 }
