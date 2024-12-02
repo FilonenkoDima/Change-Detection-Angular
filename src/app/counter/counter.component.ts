02d2326fb50f4d3ee23defa7479e88a2b81e3482
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-02 15:24:03 +0200

    going zoneless

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,8 +1,6 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
-  NgZone,
   OnInit,
   signal,
 } from '@angular/core';
@@ -17,7 +15,6 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
-  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -30,11 +27,9 @@ export class CounterComponent implements OnInit {
       this.count.set(0);
     }, 4000);
 
-    this.zone.runOutsideAngular(() => {
-      setTimeout(() => {
-        console.log('Timer expired!');
-      }, 5000);
-    });
+    setTimeout(() => {
+      console.log('Timer expired!');
+    }, 5000);
   }
 
   onDecrement() {
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,23 +1,13 @@
 import { Injectable, signal } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  messages$ = new BehaviorSubject<string[]>([]);
-  // private messages = signal<string[]>([]);
-  private messages: string[] = [];
-  get allMessages() {
-    return [...this.messages];
-  }
-  // allMessages = this.messages.asReadonly();
+  private messages = signal<string[]>([]);
+  allMessages = this.messages.asReadonly();
 
-  // addMessage(message: string) {
-  //   this.messages.update((prevMessages) => [...prevMessages, message]);
-  // }
   addMessage(message: string) {
-    this.messages = [...this.messages, message];
-    this.messages$.next([...this.messages]);
+    this.messages.update((oldMessages) => [...oldMessages, message]);
   }
 }
